6139e1bc139820edda0acaa366b6e7c4571ac389
Amaury-19 <a.viscaino.v023@edu.utc.mx>
2022-09-19 10:56:09 -0500

    clase

---
+++ 02holaMundo/Holamundo.java
@@ -0,0 +1,27 @@
+//ESTO ES UN COMENTARIO
+/*
+ * este comentario nos sirve para poder 
+ tener varias lineas comentadas del 
+codigo
+
+todo programa en java, maneja clases, una calse es un objeto del mundo real
+ */
+
+
+class holamundo{
+    //cuerpo del programa
+    /*
+     * aqui es donde nosotros codificamos todos los elementos del programa, funciones, estructuras, variables y metodos
+     * toda clase debe tener un metodo principal
+     */
+    public static void main (String[]
+    args){
+        System.out.println("Hola Mundo");
+
+
+    }
+
+}
+
+
+

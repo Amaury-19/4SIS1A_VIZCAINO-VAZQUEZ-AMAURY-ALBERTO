a22883f7457bb0831ba3671d75e5ba2997c17861
Amaury-19 <a.viscaino.v023@edu.utc.mx>
2022-10-05 20:18:02 -0500

    Tarea conversiones

--- 05 Clase/Ejercicios.java
+++ 05 Clase/Ejercicios.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
-class Ejercicios {
+public class ejercicio {
+    /**
+     * Vamos a hacer una calculadora, un conversor de unidades creacion de cuadros y
+     * de movimiento
+     */
 
-    /** Vamos a hacer una calculadora, un conversor de unidades creacion de cuadros y de movimiento */
-    
-    //los metodos son las operaciones de un objeto o de una clase, tambien llamado comportamiento
+    // los metodos son las operaciones de un objeto o de una clase, tambien llamado
+    // comportamiento
 
-    //objetos
+    // objetos
     Scanner entrada = new Scanner(System.in);
 
-    //variables globales
+    // variables globales
     char op;
 
-    //vamos a ser un metodo para el menu del programa
-    public void menu(){
-        //menu del programa
+    // vamos a ser un metodo para el menu del programa
+    public void menu() {
+        // menu del programa
         System.out.println("Ejercicios de yo para tu");
         System.out.println("a.- Calculadora");
         System.out.println("b.- Conversion de unidades");
@@ -24,31 +27,31 @@ class Ejercicios {
 
         op = entrada.next().charAt(0);
 
-        switch(op){
-            case 'a' :
-                 //implementamos metodos
+        switch (op) {
+            case 'a':
+                // implementamos metodos
                 Ejercicio1();
                 break;
-            case 'b' :
-                //implementamos metodos
-               Ejercicio2();
-               break;
-            case 'c' :
-               //implementamos metodos
-              Ejercicio3();
-              break;
-            case 'd' :
-              //implementamos metodos
-             Ejercicio4();
-             break;
-            default: 
+            case 'b':
+                // implementamos metodos
+                Ejercicio2();
+                break;
+            case 'c':
+                // implementamos metodos
+                Ejercicio3();
+                break;
+            case 'd':
+                // implementamos metodos
+                Ejercicio4();
+                break;
+            default:
                 System.out.println("Gracias por jugar :3");
         }
     }
 
-    public void Ejercicio1(){
-        //variable local
-        double num1 = 0.00, suma = 0.00, multi=1.00;
+    public void Ejercicio1() {
+        // variable local
+        double num1 = 0.00, suma = 0.00, multi = 1.00,b,divi=1;
         char operacion;
 
         System.out.println("Selecciona la operacion que desees realizar");
@@ -57,105 +60,133 @@ class Ejercicios {
         System.out.println("c.- Division ahi la hacen");
 
         operacion = entrada.next().charAt(0);
-        switch(operacion){
+        switch (operacion) {
             case 'a':
-            do{
-                System.out.println("Para detener la suma o resta ingrese el numero 0");
-                System.out.println("Escriba los numeros que desee sumar o restar");
-                num1 = entrada.nextDouble();
-                suma += num1;
-                //suma = suma + num1;
+                do {
+                    System.out.println("Para detener la suma o resta ingrese el numero 0");
+                    System.out.println("Escriba los numeros que desee sumar o restar");
+                    num1 = entrada.nextDouble();
+                    suma += num1;
+                    // suma = suma + num1;
 
-
-            }while(num1 != 0);
-            System.out.println("El resultado es: " + suma);
+                } while (num1 != 0);
+                System.out.println("El resultado es: " + suma);
                 break;
             case 'b':
-            do{
-                System.out.println("Para detener la multiplicacion ingrese el numero 0");
-                System.out.println("Escriba los numeros que desee multipliar");
+            do {
+                System.out.println("Para detener multiplicaion ingrese el numero 0");
+                System.out.println("Escriba los numeros que desee sumar o restar");
                 num1 = entrada.nextDouble();
                 multi *= num1;
-                //multi = multi * num1;
+                // multi = multi * num1;
 
+            } while (num1 != 0);
+            System.out.println("El resultado es: " + multi);
+            
+                break;
+            case 'c':do{
+                System.out.println("Para detener divicion ingrese el numero 0");
+                System.out.println("ingrese el numerador ");
+                num1 = entrada.nextDouble();
+                System.out.println("ingrese el denomidador");
+                b = entrada.nextDouble();
+                if(b !=0){
+
+                    divi/=num1;
+
+                }else{
+                    System.out.println("no se puede divider entre 0 pendejo");
+                }
 
             }while(num1 != 0);
-            System.out.println("El resultado es: " + multi);
+            System.out.println("el resultado de la diicion es: " + divi);
+            break;
+                default:
+                System.out.println("hasta la vista popo");
                 break;
-            case 'c':
-            //hacer el de division
-                break;
-            default:
-                System.out.println("Opcion no valida, ADIOS!!!");
-                break;
+
         }
 
     }
 
-    public void Ejercicio2(){
-        /*
-         * Vamos a crear un programa que se encargue de convertir de Kg a libras y gramos
-         * m/s a Km/s
-         * tarea
-         * m a yardas y millas
-         */
+    public void Ejercicio2() {
+        //vamos a crear un programa que se encarge de convertir kilogramos a libras y de metros/segundo a //
+        // tarea: m a yarda y millas//
+        double metros = 0.00, kg=0.00,velocidad=0.00, cm=100, pulgadas=39.37,gramos=1000,libra=2.20,ms=0.00;
+        double a,b,pesos=0.00,dolar=0.050,euro=0.51;
+        System.out.println("seleccione la cantidad que desee convertir a las siguientes unidades");
+        System.out.println("a. metros a cm a pulgadas");
+        System.out.println("b. kilogramos a libras y gramos");
+        System.out.println("c. m/s a km/h");
+        System.out.println("d.pesos a dolar a euro");
+        op = entrada.next().charAt(0);
 
-         double metros = 0.00; kg= 0.00, velocidad = 0.00, cm = 100.00, pulgada = 39.37, gramos = 1000.00, libra =2.20, ms = 0.00;
-         double a, b;
-         System.out.println("Seleccione la cantidad que desea convertir de acuerdo a las siguientes unidades");
-         System.out.println("a. Metros a cm y pulgadas");
-         System.out.println("b. Kilogramos a libras y gramos");
-         System.out.println("c. m/s a km/h");
-         System.out.println("d. tarea");
+        switch(op){
 
-         op = entrada.next().charAt(index: 0);
-
-         switch(op){
             case 'a':
-                System.out.println("Ingrese los metros a transformar ");
-                a = metros * cm;
-                b = metros * pulgada;
-
-                System.out.println("La cantidad en metros es:" + metros + "de m a cm son :" + a + "de m a pulgadas son:" + b);
-                break;
-            
-            
-            
+            System.out.println("ingrese los metros a transformar ");
+            metros = entrada.nextDouble();
+            a= metros*cm;
+            b=metros*pulgadas;
+            System.out.println("la cantidad en metros es" + metros + "de m a cm es: " + a + "de m a pulgadas es: " + b);
+            break;
             case 'b':
-            System.out.println("Ingrese los Kilogramos a transformar ");
-            kg =
-                a = kg * gramos;
-                b = kg * libra;
-
-                System.out.println("La cantidad en Kg es:" + kg + "de kg a gramos son :" + a + "de kg a libras son:" + b);
-                break;
+            System.out.println("ingrese los gramos a transformar ");
+            kg =entrada.nextDouble();
+            a= kg*gramos;
+            b=kg*libra;
+            System.out.println("la cantidad en gramos es" + gramos + "de kg a gramos es: " + a + "de kg a libras es: " + b);
+            break;
 
             case 'c':
-            System.out.println("Ingrese la cantidad e m/s ");
-                ms = entrada.nextDouble();
-                a = ms*(3600/1000);
+            System.out.println("ingrese la m/s a transformar ");
+            ms=entrada.nextDouble();
 
-                System.out.println("La conversion de m/s a km/h es de: " + a);
-                break;
+            a= ms*(3600/1000);
+            System.out.println("la conversion es"+ a );
+            break;
+            case 'd':
+            System.out.println("ingrese los pesos a convertir");
+            pesos = entrada.nextDouble();
+            a=pesos*dolar;
+            b=pesos*euro;
+            System.out.println("la cantidad de pesos es:"+pesos+" en dolares es:"+dolar+"en euro es: "+euro);
+            break;
+        }
 
-                case 'd':
-                //tarea
+    }
 
-                case 'e'
-
-
+    public void Ejercicio3() {
+        /* */
+    int lado;
+    char letra;
+        do{
+            System.out.println("Digita el tamaño del cuadrado : "); 
+            lado = entrada.nextInt(); 
+            if(lado>=1 & lado<=1000){
               
+            for (int i=1; i<= lado; i++){
+                for(int j=1; j<=lado;j++){
+                    System.out.print(" *"); 
+                }
+                System.out.println(""); 
+                for(int k=1; k<=lado;k++){
+                System.out.print(" /"); 
+                }
+                System.out.println(""); 
+            }
+            
+        }else{
+            System.out.println("habiendo tantos numeros escogiste uno negativo y mayor a mill, seras pelotudo");
+        }
+              System.out.println("Deseas repetir el programa, escribe s para si");
+                  letra = entrada.next().charAt(0);
+        }while(letra == 's' || letra == 'S' );
 
-         }
-  
     }
 
-    public void Ejercicio3(){
-        
-    }
+    public void Ejercicio4() {
 
-    public void Ejercicio4(){
-        
     }
 
 }
